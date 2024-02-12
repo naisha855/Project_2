64c1089a9a760829eb2e5457484bd049749bd2c3
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-02-13 03:19:21 +0900

    2024-02-13_00001

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.min.js"
-(function()){
 
+
+$(".nav_bars").click(function(){
+    $(".menu").slideToggle();
+    });
+    $(window).resize(function(){
+    var wWidth = $(window).width();
+    if(wWidth > 400 && $(".menu").is(":hidden")){
+    $(".menu").removeAttr("style");
+    }
+    });
+
+
+
+var ct=0;
+var imgs= new Array("img/cook-2364221_1280.jpg", "img/banner2.jpg", "img/banner3.jpg")
+
+setTimeout(progress, 1000);
+
+function progress(){
+document.images[1].src=imgs[ct];
+ct++;
+setTimeout(progress, 5000);
+if(ct>2){
+    ct=0;
 }
+};
+
