985240d05ef7fb6460f141ad4aa52fb1ecd8a981
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-02-19 17:36:31 +0900

    2024-02-19_01

--- js/common.js
+++ js/common.js
@@ -1,34 +1,12 @@
-$(".nav_bars").click(function(){
-    $("nav ul li").slideToggle();
-    });
-    $(window).resize(function(){
-    var wWidth = $(window).width();
-    if(wWidth > 400 && $("nav ul li").is(":hidden")){
-    $("nav ul li").removeAttr("style");
-    }
-    });
-
-
-
-var ct = 0;
-var imgs = new Array("img/cook-2364221_1280.jpg", "img/banner2.jpg", "img/banner3.jpg")
-
-setTimeout(progress, 3000);
-
-function progress() {
-    document.getElementById("slider_img").src = imgs[ct];
-    ct++;
-    setTimeout(progress, 3000);
-    if (ct > 2) {
-        ct = 0;
-    }
-};
-
-$(function(){
-    $('#reservation_click').click(function(){
-        $('#modalwrap').fadeIn();
-    });
-    $('#reservation_close').click(function(){
-        $('#modalwrap').fadeOut();
-    });
+$(".nav_bars").click(function () {
+    $("nav").slideToggle();
 });
+
+
+$(window).resize(function () {
+    var wWidth = $(window).width();
+    if (wWidth > 400 && $("nav ul li").is(":hidden")) {
+        $("nav ul li").removeAttr("style");
+    }
+});
+
--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 
-$(".nav_bars").click(function(){
-    $("nav ul li").slideToggle();
-    });
-    $(window).resize(function(){
-    var wWidth = $(window).width();
-    if(wWidth > 400 && $("nav ul li").is(":hidden")){
-    $("nav ul li").removeAttr("style");
-    }
-    });
+
 
 
 
